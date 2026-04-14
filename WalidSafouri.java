24612cd1798b6de75c67f88df29cf1c8c7ed752c
safouri <walidsafoury@gmail.com>
2026-04-14 13:19:03 +0300

    change in WalidSafouri

--- WalidSafouri.java
+++ WalidSafouri.java
@@ -1,7 +1,7 @@
 public class WalidSafouri {
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
-            System.out.println("My name is Walid Safouri");
+            System.out.println("My name is Walid Safouri!!!");
             }
     }
 }
